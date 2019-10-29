12ec7d97b1882d6ca22b75d3bdd5c276abbefa03
Andres Ramirez Fonseca <andresr@solin.com.co>
2019-10-29 17:25:38 -0500

    Importación proyecto
    Se realiza la importación del proyecto a GitHub

---
+++ Autentificacion/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Autentificacion.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Autentificacion.Areas.Identity.IdentityHostingStartup))]
+namespace Autentificacion.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
